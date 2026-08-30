98b394843bb7840d758d2f205a98c87cdb88d7e9
Dwarapudi Suswetha <dwarapudisuswetha.24.csm@anits.edu.in>
2026-08-30 21:31:04 +0530

    Delete record/exp4 directory

--- record/exp4/exp4.js
+++
@@ -1,9 +0,0 @@
-function changeContent() {
-    let message = document.getElementById("message");
-
-    message.innerHTML = "You clicked the button!";
-    message.style.color = "red";
-    message.style.fontWeight = "bold";
-
-    document.body.style.backgroundColor = "#d4edda";
-}
